00a3e3c3490822c528fe3d3432862560ad3dae8c
Tipog7 <116251713+Tipog7@users.noreply.github.com>
2022-11-10 13:26:59 +0700

    Create TestJS.js

---
+++ week4/TestJS.js
@@ -0,0 +1,11 @@
+console.log(5);
+var myName ='Tipog';
+let lastName ='Boonyanupong';
+
+const year = 2002;
+
+let grade = 3.56;
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`My name is ${myName}.My lastName is ${lastName}.`)
