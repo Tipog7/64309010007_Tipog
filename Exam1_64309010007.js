80fd626d6cf3805254c847308e6d0d0cb4229e27
Tipog7 <116251713+Tipog7@users.noreply.github.com>
2022-11-10 13:31:06 +0700

    Update Exam1_64309010007.js

--- Exam1_64309010007.js
+++ Exam1_64309010007.js
@@ -3,4 +3,7 @@ const Celsius = kelvin - 273;
 let Fahrenheit = Celsius * (9/5) + 32;
 
 Fahrenheit = Math.floor(Fahrenheit);
-console.log(`The temperature is >${Fahrenheit}< degrees Fahrenheit`);
+
+console.log('---------------------vv----------------------');
+console.log(`The temperature is > ${Fahrenheit} < degrees Fahrenheit`);
+console.log('---------------------^^----------------------');
